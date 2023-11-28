c9b9cb5974734f6ed098b9e267808cdd84d26a95
giftthedeveloper <giftjeremiah001@gmail.com>
2023-11-28 14:33:51 +0100

    feat: created a channel for testing

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let localStream;
+const app_id = "438b0a9bb09b4df992cad2765963018d"
+const token = null; 
+const uid = String(Math.floor(Math.random() * 10000 ))
+
+let client;
+let channel;
 
 const servers = {
     iceServers:[
@@ -9,19 +15,36 @@ const servers = {
 }
 
 const init = async() => {
+    const client = await AgoraRTM.createInstance(app_id)
+    await client.login({uid, token})
+
+    // channel = client.createChannel(roomID)
+    channel = client.createChannel('main')
+    await channel.join()
+
+    channel.on('MemberJoined', handleUserJoined)
+
+
      localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false}) //change the audio back to true when testing with another device
     console.log("here")
     document.getElementById('client-1').srcObject = localStream
     
     
-    createOffer()
-    console.log("called the function")
+    // createOffer()
+    // console.log("called the function")
 
 
 }
 
+const handleUserJoined = async(MemberId)=> {
+
+    console.log('A new user joined the rom', MemberId)
+    createOffer(MemberId)
+    console.log("called the function")
+}
+
 //create sdp offer for peer connection
-const createOffer = async () => {
+const createOffer = async (MemberId) => {
     const peerConnection = new RTCPeerConnection(servers)
     const remoteStream = new MediaStream()
     document.getElementById('client-2').srcObject = remoteStream
